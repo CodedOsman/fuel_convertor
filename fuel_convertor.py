5045875d235ab04534870dcf7f46b79341347a66
Osman Ibrahim <41456904+Vipah1@users.noreply.github.com>
2024-04-08 09:42:26 -0600

    Comments updated

--- fuel_convertor.py
+++ fuel_convertor.py
@@ -1,3 +1,4 @@
+''' Loading libraries '''
 from tkinter import Tk, ttk
 from tkinter import *
 
@@ -5,10 +6,12 @@ from PIL import Image, ImageTk
 import requests
 import json
 
+''' colors to be used '''
 color0 = "#FFFFFF"
 color1 = "#333333"
 color2 = "#E85051"
 
+''' Calling UI library to set window parameters '''
 window = Tk()
 window.geometry('300x320')
 window.title('Fuel Convertor')
@@ -21,6 +24,7 @@ top.grid(row=0, column=0)
 main = Frame(window, width=300, height=260, bg=color0)
 main.grid(row=1, column=0)
 
+''' function to load API '''
 def convert():
     url = "https://currency-converter18.p.rapidapi.com/api/v1/convert"
 
@@ -49,22 +53,27 @@ app_name = Label(top, image=icon, compound=LEFT, text="FUEL CONVERTER", height=5
 app_name.place(x=0, y=0)
 
 #main frame
+#label to display converted value
 result = Label(main, text=" ", width=16, height=2, pady=7, relief="solid", font=('Ivy 15 bold'), anchor=CENTER,bg=color0, fg=color1)
 result.place(x=50, y=10)
 
+#preset supported currencies
 curr_vale = ['CAD', 'GHS', 'USD', 'INR', 'GBP', 'JPY']
 
+#label and options for currency
 currency = Label(main, text="Currency", width=8, height=1, padx=0, pady=0, relief="flat", font=('Ivy 10'), anchor=NW,bg=color0, fg=color1)
 currency.place(x=48, y=90)
 combo1 = ttk.Combobox(main, width=8, justify=CENTER, font=('Ivy 12 bold'))
 combo1['values'] = (curr_vale)
 combo1.place(x=50, y=115)
 
+#label and text area for amount input
 amount_label = Label(main, text="Amount", width=8, height=1, padx=0, pady=0, relief="flat", font=("Ivy 10"), anchor=NW, bg=color0, fg=color1)
 amount_label.place(x=158, y=90)
 amount = Entry(main, width=12, justify=CENTER, font=('Ivy 12 bold'))
 amount.place(x=160, y=115)
- 
+
+#button to do conversion by calling API function
 button = Button(main, text='CONVERT', justify=CENTER, width=15, padx=5, height=1, bg=color2, fg=color0, font=('Ivy 15 bold'), command=convert)
 button.place(x=50, y=210)
 
